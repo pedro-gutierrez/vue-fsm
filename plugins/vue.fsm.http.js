281d82b2d6777273eb7bd66c70b2a100bbfc1903
Pedro Gutierrez <pedrogutierrez@mac.com>
2016-06-21 02:40:20 +0200

    http plugin

---
+++ plugins/vue.fsm.http.js
@@ -0,0 +1,76 @@
+(function() {
+
+  Vue.$plugin({
+    dsl: {
+      specs: [ "post", "get" ],
+      fun: function( fsm, spec ) {
+        return function() {
+          var opts = {};
+          if( spec.body ) opts.data=Vue.$resolveSpec( spec.body, fsm.$data );
+          opts.path = spec.get || spec.post;
+          opts.method = spec.post ? "POST" : "GET";
+          opts.binary = spec.binary;
+          opts.raw = spec.raw;
+          fsm.$http( opts );
+        }
+      }
+    },
+    methods: {
+      
+      $http: function(opts) {
+        var owner = this;
+        var dataType = opts.type || 'json';
+        var xhr = {
+          type:  opts.method || 'GET',
+          url: opts.path,
+          data: opts.data,
+          dataType: dataType,
+          success: function(data, status, resp){
+            printResponse( owner, dataType, data );
+            opts.success ? opts.success( data ) : owner.$receive( 'data', data );
+          },
+          error: function(e) {
+            console.log( e.status );
+            var msg = e.responseJSON ? e.responseJSON : {};
+            printResponse( owner, dataType, msg );
+            if( opts.error ) return opts.error(msg);
+            msg.status = http_codes[e.status] || 'error';
+            owner.$receive( msg.status, msg );
+          }
+        };
+
+        if( httpDebug ) owner.$debug( "-> " + xhr.type + " " + xhr.url + " " + ( opts.data ? JSON.stringify( opts.data) : '' ) );
+        if ( xhr.type === "GET" && xhr.data && opts.binary ) xhr.data = xhr.data.$qs();
+
+        if( xhr.dataType === 'json' ) {
+          xhr.contentType = "application/json";
+          if( xhr.type === "POST" ) xhr.data = JSON.stringify(xhr.data);
+        }
+
+        $.ajax(xhr);
+      }
+    }
+  });
+
+  var http_codes = {
+    400: 'invalid',
+    401: 'forbidden',
+    404: 'not_found',
+    409: 'conflict',
+    403: 'forbidden',
+    503: 'error',
+    500: 'error',
+    501: 'not_implemented'
+  }
+
+  var httpDebug = function() {
+    if( Vue.$config.http ) return Vue.$config.http.verbose;
+    return false;
+  };
+
+  var printResponse = function (owner, dataType,  msg) {
+    if( httpDebug ) owner.$debug( "<- " + (dataType === "json" ? JSON.stringify(msg) : msg) );
+  }
+
+
+})();
